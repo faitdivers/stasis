ab0336db5baea8f602c0f4f07ae80ba877491c9f
davidjacnogueira <david_alberto_nogueira@hotmail.com>
2017-01-29 12:48:13 +0000

    Move file folder.

---
+++ inc/topinclude.hpp
@@ -0,0 +1,18 @@
+#ifndef _DEF_TOPINCLUDE_H_
+#define _DEF_TOPINCLUDE_H_
+
+#if defined(_MSC_VER)
+#   define _STASIS_WINDOWS
+#   define _STASIS_OSNAME "Windows"
+#endif
+
+#ifdef __linux__
+#   define _STASIS_LINUX
+#   define _STASIS_OSNAME "Linux"
+#endif
+
+#ifdef _STASIS_LINUX
+#include <assert.h>
+#endif
+
+#endif  //_DEF_TOPINCLUDE_H_
